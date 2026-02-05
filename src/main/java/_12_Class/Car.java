ffc02525e9ebce91e33314086a3a05d5bc0c5052
jh10042 <gimj87308@gmail.com>
2026-02-05 17:38:35 +0900

    클래스

---
+++ src/main/java/_12_Class/Car.java
@@ -0,0 +1,40 @@
+package _12_Class;
+
+public class Car {
+    String brand;
+    String model;
+    int year;
+
+    // 항상 변할 수 있다고 가정
+    int speed;
+    boolean isEngineOn;
+
+    // (인스턴스) 메서드 - static이 없으면
+    // 객체의 필드값을 메서드 내에서 사용가능/조작 OK
+    void engineStart() {
+        isEngineOn = true;
+        System.out.println(model + "엔진 on!");
+
+    }
+
+    // 객체의 필드값에 따라 다르게 작동하게 할 수 있음
+    void accel() {
+        if (!isEngineOn) {
+            System.out.println("시동을 먼저 켜주세요");
+            return;
+        }
+        speed += 20;
+        System.out.println("현재 속도: " + speed);
+    }
+
+    // 1)
+    public Car(String brand, String model, int year, int speed, boolean isEngineOn) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.speed = speed;
+        this.isEngineOn = isEngineOn;
+    }
+
+    public Car() {}
+}
